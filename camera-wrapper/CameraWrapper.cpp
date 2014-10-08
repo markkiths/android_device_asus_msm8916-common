a24f833cff2dfae523e883ca8a65e314565bae57
Arne Coucheron <arco68@gmail.com>
2014-10-08 11:03:00 -0500

    msm8916-common: camera-wrapper: Update HAL API version
    Change-Id: I10c802d4c8ef480797f709bbc58770ceae914aa9

--- camera-wrapper/CameraWrapper.cpp
+++ camera-wrapper/CameraWrapper.cpp
@@ -65,8 +65,8 @@ static struct hw_module_methods_t camera_module_methods = {
 camera_module_t HAL_MODULE_INFO_SYM = {
     .common = {
         .tag = HARDWARE_MODULE_TAG,
-        .module_api_version = CAMERA_MODULE_API_VERSION_1_0,
-        .hal_api_version = HARDWARE_HAL_API_VERSION,
+        .version_major = 1,
+        .version_minor = 0,
         .id = CAMERA_HARDWARE_MODULE_ID,
         .name = "MSM8916 Camera Wrapper",
         .author = "The CyanogenMod Project",
@@ -616,7 +616,7 @@ static int camera_device_open(const hw_module_t *module, const char *name,
         memset(camera_ops, 0, sizeof(*camera_ops));
 
         camera_device->base.common.tag = HARDWARE_DEVICE_TAG;
-        camera_device->base.common.version = CAMERA_DEVICE_API_VERSION_1_0;
+        camera_device->base.common.version = CAMERA_MODULE_API_VERSION_1_0;
         camera_device->base.common.module = (hw_module_t *)(module);
         camera_device->base.common.close = camera_device_close;
         camera_device->base.ops = camera_ops;
