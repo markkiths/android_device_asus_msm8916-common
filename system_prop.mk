106dd347bd0b8a1dc60d6faa2e1965f2350c40c8
Christian Oder <myself5@carbonrom.org>
2016-05-28 00:34:53 +0200

    msm8916-common: remove rild.libargs
    As discussed, it is not needed, and doesnt work anyway, as spaces in
    overrides do not work and results into the build.prop entry being:
    
    rild.libargs=-d
    /dev/smd0

--- system_prop.mk
+++ system_prop.mk
@@ -160,7 +160,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.dont_use_dsd=true \
     persist.radio.flexmap_type=dds \
     persist.radio.multisim.config=dsds \
-    rild.libargs=-d /dev/smd0 \
     rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
     ril.subscription.types=NV,RUIM \
     ro.telephony.call_ring.multiple=false \
