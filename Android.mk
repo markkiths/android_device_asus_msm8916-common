0de6e207bd77c65bef5f5f162e741ace5d4f13c5
Ad!thya R <gh0strider.2k18.reborn@gmail.com>
2018-10-02 17:25:39 +0000

    msm8916-common: Move keymaster firmware to /vendor/firmware
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- Android.mk
+++ Android.mk
@@ -45,6 +45,18 @@ $(ISDB_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 MSM8916_SYMLINKS += $(ISDB_SYMLINKS)
 
+KM_IMAGES := \
+    keymaste.b00 keymaste.b01 keymaste.b02 keymaste.b03 keymaste.mdt
+
+KM_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(KM_IMAGES)))
+$(KM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Keymaster firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+MSM8916_SYMLINKS += $(KM_SYMLINKS)
+
 MBA_IMAGES := mba.mbn
 
 MBA_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(MBA_IMAGES)))
