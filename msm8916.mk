977568b812bd829ef99f6009672310b6ba49f714
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2018-11-01 20:03:02 +0000

    msm8916-common: rootdir: Rename FM prop for new HAL
    * Needed to fix FM after LineageOS/android_hardware_qcom_fm@131be18.
    
    Partially reverts 0763c95.
    
    Change-Id: I64ac891940aadc7662cd15d6119955f6505b297e
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -70,7 +70,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.1-impl \
     android.hardware.soundtrigger@2.1-service \
     android.hardware.bluetooth.a2dp@1.0-impl
