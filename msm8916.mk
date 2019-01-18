02feb7906b20dc2461a7a144b97b1c32221aed53
Paul Keith <javelinanddart@gmail.com>
2019-01-18 23:36:25 +0100

    msm8916-common: Build livedisplay 2.0 service
    * Only IPictureAdjustment is added because only that is supported
    
    Change-Id: Ia96dfe341d07c9f79b1ab5bd19af0e32bee4b4ed

--- msm8916.mk
+++ msm8916.mk
@@ -203,7 +203,7 @@ endif
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm
 
 # Media
 PRODUCT_COPY_FILES += \
