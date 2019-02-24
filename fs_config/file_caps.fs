8a5ab26eac995056ddbb8a0d97d862219559ee57
Kyle Harrison <khwebmail@gmail.com>
2019-02-24 12:23:30 +0000

    msm8916-common: Add IMS stack
    This is a kang of the IMS implementation from https://github.com/nicorg2515/android_device_motorola_msm8916-common/tree/lineage-16.0
    
    All credits to https: //github.com/nicorg2515 for getting this stack working in Pie.
    Change-Id: I2310b01a30b329dbf154bd96d59825d5631afcce
    
    Change-Id: I7d225d7aa18c113dc4b1fb9ae13480759082c0e6

--- fs_config/file_caps.fs
+++ fs_config/file_caps.fs
@@ -31,6 +31,12 @@ group: AID_SYSTEM
 mode: 0755
 caps: NET_BIND_SERVICE
 
+[vendor/bin/ims_rtp_daemon]
+user: AID_SYSTEM
+group: AID_RADIO
+mode: 0755
+caps: NET_BIND_SERVICE
+
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
