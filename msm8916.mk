cb7f0057556d46e79b4f18ba35ab791a889dba64
RahifM <faizel326@gmail.com>
2019-07-28 16:04:40 +0530

    wt88047: Add Dirac support [1/2]
    * Kanged from OP5T-AOSiP: https://github.com/DerpFest-OP5T/platform_packages_apps_Dirac
    
    Signed-off-by: RahifM <faizel326@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -144,6 +144,10 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# Dirac
+PRODUCT_PACKAGES += \
+    Dirac
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
