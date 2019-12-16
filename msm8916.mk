c3a53c984e1867fae87ec002e0a3ab36095a6ae4
ghostrider-reborn <gh0strider.2k18.reborn@gmail.com>
2019-12-17 02:01:25 +0530

    msm8916-common: Remove soundtrigger HAL
    * Sound trigger doesnt work well on msm8916 since O
    * Our HAL fails to load anyway as it has missing atomic symbols
    
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -69,11 +69,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl \
-    android.hardware.soundtrigger@2.1-service \
-    android.hardware.bluetooth.a2dp@1.0-impl
-
+    android.hardware.audio.effect@5.0-impl
+    
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
