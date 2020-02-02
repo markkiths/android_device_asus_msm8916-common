80566b8aa44929862759791b44de6bb5b2793e7c
android-usb <pmclean@google.com>
2020-02-02 16:25:15 +0100

    msm8916-common: Uprev audio HAL to v5

--- msm8916.mk
+++ msm8916.mk
@@ -60,9 +60,9 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing
 
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.broadcastradio@1.0-impl \
     android.hardware.soundtrigger@2.0-impl \
     android.hardware.soundtrigger@2.0-service
@@ -170,7 +170,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
-	
+
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8916 \
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl \
     android.hardware.renderscript@1.0-service
-	
+
 # RIL
 PRODUCT_PACKAGES += \
     librmnetctl \
@@ -334,7 +334,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libwcnss_qmi \
     libwpa_client
-    
+
 PRODUCT_PACKAGES += \
     wcnss_service
 
