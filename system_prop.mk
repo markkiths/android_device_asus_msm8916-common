2ec415ca227ffffdbfc2c055e9c02df59170a7e5
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    msm8916-common: Use dex2oat64
    Enable dex2oat64 use.
    
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095

--- system_prop.mk
+++ system_prop.mk
@@ -1,7 +1,8 @@
-# Art
+# Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
-    dalvik.vm.dex2oat-swap=false
+    dalvik.vm.dex2oat-swap=false \
+    dalvik.vm.dex2oat64.enabled=true
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
