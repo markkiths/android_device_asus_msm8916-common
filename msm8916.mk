e7af9d8afba738a83ea69980802af23c643a3c14
Alexander <yaalex@yaalex.tk>
2020-06-06 21:17:32 +0530

    msm8916-common: thermal: Remove unused

--- msm8916.mk
+++ msm8916.mk
@@ -322,7 +322,6 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/thermal/thermal-engine-8916.conf:system/etc/thermal-engine-8916.conf \
-    $(LOCAL_PATH)/thermal/thermal-engine-8939.conf:system/etc/thermal-engine-8939.conf
 
 # USB HAL
 PRODUCT_PACKAGES += \
