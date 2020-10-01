a05090774b701f6e3a9ebab38ddb8d002b1ca818
LuK1337 <priv.luk@gmail.com>
2020-10-01 11:52:48 +0100

    msm8916-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- msm8916.mk
+++ msm8916.mk
@@ -204,7 +204,9 @@ PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.manager@1.0
+    android.hidl.manager@1.0 \
+    libhidltransport \
+    libhwbinder
 
 # IMS
 PRODUCT_PACKAGES += \
