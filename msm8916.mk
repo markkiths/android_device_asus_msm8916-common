7915520a25c94461aab393f7ff286d94f6c2c65a
z3DD3r <2339201+z3DD3r@users.noreply.github.com>
2021-01-23 19:25:55 +0300

    msm8916-common: fastcharge: Build FastCharge HAL

--- msm8916.mk
+++ msm8916.mk
@@ -382,6 +382,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# FastCharge
+PRODUCT_PACKAGES += \
+    vendor.lineage.fastcharge@1.0-service.usb
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
