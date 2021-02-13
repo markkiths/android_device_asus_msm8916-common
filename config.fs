5cdc20ef88125c00e0bb180cb79521417754dcb3
markkiths <markkiths@gmail.com>
2021-02-13 21:26:12 +0700

    Revert "msm8916-common: Kill IMS"
    This reverts commit bcc854136f510c1e0e1e5f6f50e3366f5850faae.

--- config.fs
+++ config.fs
@@ -19,6 +19,18 @@ user: AID_SYSTEM
 group: AID_SYSTEM
 caps: NET_BIND_SERVICE
 
+[system/vendor/bin/imsdatadaemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
+
+[system/vendor/bin/ims_rtp_daemon]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE
+
 [factory/]
 mode: 0775
 user: AID_SYSTEM
