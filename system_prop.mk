8420ef741601acfa4244bae8ea1710c1fc5da69c
markkiths <markkiths@gmail.com>
2021-03-16 22:43:21 +0700

    clean up system prop
    missed in https://github.com/markkiths/android_device_asus_msm8916-common/commit/f857cd574feb024009eb80c09bb1f4f28693907f#diff-7234478be6486d34f8dca5cafdb792a0a9a753d829e2d24cc8b947080b260990
    Thanks @Roker2

--- system_prop.mk
+++ system_prop.mk
@@ -158,7 +158,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.empty_app_percent=50 \
     ro.vendor.qti.sys.fw.trim_empty_percent=100 \
     ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-    ro.vendor.qti.sys.fw.trim_enable_memory=1073741824 \
+    ro.vendor.qti.sys.fw.trim_enable_memory=1073741824
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
