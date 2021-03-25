667a1f40fcb5e21d6658073d19231fb6b454d66f
markkiths <markkiths@gmail.com>
2021-03-26 02:01:49 +0700

    msm8916-common: rootdir: Update post_boot script from https://github.com/ghostrider-reborn/android_device_lenovo_A6020/blob/quartz/rootdir/etc/init.qcom.post_boot.sh
    This reverts commit cba4cbf4bac0e7b2eae2eb5a541f9bc8ca6a536e.

--- msm8916.mk
+++ msm8916.mk
@@ -24,7 +24,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Permissions
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
@@ -293,7 +292,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.opengles.sh \
-    init.qcom.power.sh \
+    init.qcom.post_boot.sh \
     init.qcom.bt.sh \
     init.qcom.factory.sh
 
