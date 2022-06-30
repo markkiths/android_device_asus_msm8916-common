d56450088d0141599cc0898de86121b249585d6d
markkiths <markkiths@gmail.com>
2022-06-30 19:13:53 +0000

    add BOARD_VENDOR_SEPOLICY_DIRS

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -196,6 +196,7 @@ USE_OPENGL_RENDERER := true
 
 # SELinux
 BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy/vendor
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
