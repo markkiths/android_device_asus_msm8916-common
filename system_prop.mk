79d5432ec5155f9ce99c84953efffb6e63262a7c
Redick Lin <redicklin@google.com>
2022-07-19 17:56:00 +0530

    msm8916-common: Setup and use sf offsets as durations
    In Android R, the SurfaceFlinger calculate phase offsets
    based on duration. Adopt the new way of specifying
    phase offsets to SurfaceFlinger.
    
    SamarV-121: configs kanged from RMX2151_11_F.01
    Fixes lag while playing 1080p/2k video on youtube
    
    sarthakroy2002: configs kanged from RMX3193_11_A.01
    
    Change-Id: Ia717f5c0075b6adaedaf20dd6be689737edc1415

--- system_prop.mk
+++ system_prop.mk
@@ -82,9 +82,20 @@ PRODUCT_PROPERTY_OVERRIDES += \
    persist.hwc.mdpcomp.enable=true \
    persist.hwc.ptor.enable=true \
    persist.hwc.pubypass=true \
-   debug.sdm.support_writeback=0 \
+   debug.sdm.support_writeback=0
+
+# SurfaceLinger
+PRODUCT_PROPERTY_OVERRIDES += \
    ro.surface_flinger.max_frame_buffer_acquired_buffers=1 \
-   ro.surface_flinger.force_hwc_copy_for_virtual_displays=true
+   ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+   debug.sf.use_phase_offsets_as_durations=1 \
+   debug.sf.late.sf.duration=27600000 \
+   debug.sf.late.app.duration=20000000 \
+   debug.sf.early.sf.duration=27600000 \
+   debug.sf.early.app.duration=20000000 \
+   debug.sf.earlyGl.sf.duration=27600000 \
+   debug.sf.earlyGl.app.duration=20000000 \
+   debug.sf.hwc.min.duration=17000000
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
