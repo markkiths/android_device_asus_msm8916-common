Andromax-Devices/android_device_cyanogen_msm8916-common

msm8916-common: sepolicy: Allow system_app to access storaged via IPC
avc: denied { call } for comm=4173796E635461736B202333
scontext=u:r:system_app:s0 tcontext=u:r:storaged:s0 tclass=binder
permissive=0

Change-Id: I933dcebf2f5960d639ce47be379f62636e4ddd69
Signed-off-by: dodyirawan85 <40514988+dodyirawan85@users.noreply.github.com>

BoardConfigCommon.mk
@@ -135,8 +135,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(PLATFORM_PATH)/sepolicy/private
 
 # Shims
 TARGET_LD_SHIM_LIBS := \

markkiths/android_device_asus_msm8916-common

BoardConfigCommon.mk
@@ -185,6 +185,7 @@ USE_OPENGL_RENDERER := true
 
 # SELinux
 BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(VENDOR_PATH)/sepolicy/private
 include device/qcom/sepolicy-legacy/sepolicy.mk
 
 # Dexpreopt