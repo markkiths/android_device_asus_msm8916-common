84af1fc6ae935666f4619bba9e984b4b17b3c441
markkiths <markkiths@gmail.com>
2023-07-23 14:14:14 +0700

    msm8916-common: Configuring ART https://source.android.com/docs/core/runtime/configure

--- system_prop.mk
+++ system_prop.mk
@@ -3,6 +3,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
    dalvik.vm.dex2oat-flags=--no-watch-dog \
    dalvik.vm.dex2oat-threads=1 \
    dalvik.vm.boot-dex2oat-threads=1 \
+   dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3 \
+   dalvik.vm.image-dex2oat-threads=2 \
+   dalvik.vm.image-dex2oat-cpu-set=0,1,2,3 \
+   dalvik.vm.dex2oat-cpu-set=0,1,2,3 \
    dalvik.vm.dex2oat-swap=false \
    dalvik.vm.dex2oat64.enabled=true
 
